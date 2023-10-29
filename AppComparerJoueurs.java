6b4aeb858c43718b3fac56892d1758fb75dfe53c
timotheeFert <148551228+timotheeFert@users.noreply.github.com>
2023-10-29 15:00:36 +0100

    AppComparerJoueurs.java

---
+++ AppComparerJoueurs.java
@@ -0,0 +1,14 @@
+package fr.gns.app;
+
+public class AppComparerJoueurs {
+	
+	public static void main(String[] args) {
+		Joueur jr1 = new Joueur(28, "Parker", "France", 30);
+		Joueur jr2 = new Joueur(28, "Parker", "France", 30);
+		System.out.println("Premier joueur : " + jr1);
+		System.out.println("Second joueur : " + jr2);
+		
+		System.out.println(jr1.compareTo(jr2));
+	}
+}
+
