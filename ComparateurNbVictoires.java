11be3bd169dbda9c21dc0e282be10e0507a3597e
timotheeFert <148551228+timotheeFert@users.noreply.github.com>
2023-11-06 15:03:24 +0100

    ComparateurNbVictoires.java

--- ComparateurNbVictoires.java
+++ ComparateurNbVictoires.java
@@ -1,39 +1,16 @@
 package fr.gns.app;
 
-public class ComparateurNbVictoires {
+import java.util.Comparator;
 
-	public static void main(String[] args) {
-		Joueur jr1 = new Joueur(28, "Parker1", "France", 11);
-		Joueur jr2 = new Joueur(28, "Parker2", "France", 5);
-		Joueur jr3 = new Joueur(28, "Parker3", "France", 258);
-		Joueur jr4 = new Joueur(28, "Parker4", "France", 27);
-		Joueur jr5 = new Joueur(28, "Parker5", "France", 9);
-		
-		System.out.println("Premier joueur : " + jr1);
-		System.out.println("Deuxieme joueur : " + jr2);
-		System.out.println("Troisieme joueur : " + jr3);
-		System.out.println("Quatrieme joueur : " + jr4);
-		System.out.println("Cinquieme joueur : " + jr5);
-		
-		Joueur joueurAvecMoinsDeVictoires = null;
+public class ComparateurNbVictoires implements Comparator<Joueur> {
 
-        // Traiter les joueurs un par un et mettre à jour le joueur avec le moins de victoires
-		   joueurAvecMoinsDeVictoires = comparerJoueur(joueurAvecMoinsDeVictoires, jr1);
-	     joueurAvecMoinsDeVictoires = comparerJoueur(joueurAvecMoinsDeVictoires, jr2);
-	     joueurAvecMoinsDeVictoires = comparerJoueur(joueurAvecMoinsDeVictoires, jr3);
-	     joueurAvecMoinsDeVictoires = comparerJoueur(joueurAvecMoinsDeVictoires, jr4);
-	     joueurAvecMoinsDeVictoires = comparerJoueur(joueurAvecMoinsDeVictoires, jr5);
-        
-	   System.out.println( "Joueur avec le moins de victoires : " + joueurAvecMoinsDeVictoires.getNom() + " avec " + joueurAvecMoinsDeVictoires.getNbVictoires() + " victoires" );
-	   
-	    }
-         
-	   //implementation de comparerJoueur
-	public static Joueur comparerJoueur(Joueur joueurActuel, Joueur nouveauJoueur) {
-        if (joueurActuel == null || nouveauJoueur.getNbVictoires() < joueurActuel.getNbVictoires()) {
-            return nouveauJoueur;
-        }
-        return joueurActuel;
+	public int compare(Joueur o1, Joueur o2) {
+		    if (o1.getNbVictoires() > o2.getNbVictoires()) {
+		        return 1;
+		    } else if (o1.getNbVictoires() == o2.getNbVictoires()) {
+		        return 0;
+		    } else {
+		        return -1;
+		    }
+		}
 	}
-	
-}
