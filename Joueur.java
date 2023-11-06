81d45c22c555f14faa6966926cc03d24a3d546c5
timotheeFert <148551228+timotheeFert@users.noreply.github.com>
2023-11-06 15:03:42 +0100

    Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,18 +1,26 @@
 package fr.gns.app;
 
-	class Joueur implements Comparable<Joueur> {
-		private int numero ;
-		private String nom ;
-		private String pays ;
-		private int nbVictoires = 0 ;
-	
-	public Joueur( int numero , String nom , String pays , int nbVictoires ){
-		super() ;
-		this.numero = numero ;
-		this.nom = nom ;
-		this.pays = pays ;
-		this.nbVictoires = nbVictoires ;
+import java.util.Objects;
+
+public class Joueur {
+	private int numero ;
+	private String nom ;
+	private String pays ;
+	private int nbVictoires = 0 ; // Nombre de victoires remportées
+	public Joueur( int numero , String nom , String pays ){
+	super() ;
+	this.numero = numero ;
+	this.nom = nom ;
+	this.pays = pays ;
 	}
+	public Joueur( int numero , String nom , String pays , int nbVictoires ){
+	super() ;
+	this.numero = numero ;
+	this.nom = nom ;
+	this.pays = pays ;
+	this.nbVictoires = nbVictoires ;
+	}
+	
 	public int getNumero() {
 		return numero;
 	}
@@ -38,31 +46,21 @@ package fr.gns.app;
 		this.nbVictoires = nbVictoires;
 	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Joueur other = (Joueur) obj;
+		return nbVictoires == other.nbVictoires && Objects.equals(nom, other.nom) && numero == other.numero
+				&& Objects.equals(pays, other.pays);
+	}
+	@Override
 	public String toString() {
-		return "Joueur [numero=" + numero + ", nom=" + nom + ", pays=" + pays + ", nbVictoires=" + nbVictoires + "]";
+		return "numero = " + numero + ", nom = " + nom + ", pays = " + pays + ", nbVictoires = " + nbVictoires + "\n";
 	}
 	
-	 public boolean equals(Joueur autreJoueur) {
-	        if (this == autreJoueur) {
-	            return true; 
-	        }
-
-	        if (autreJoueur == null) {
-	            return false; 
-	        }
-
-	        return numero == autreJoueur.numero;
-	  }
-	 
-	 public int compareTo(Joueur autreJoueur) {
-		    if (this.getNbVictoires() > autreJoueur.getNbVictoires()) {
-		        return 1;
-		    } else if (this.getNbVictoires() == autreJoueur.getNbVictoires()) {
-		        return 0;
-		    } else {
-		        return -1;
-		    }
-	}
-	 
-	 
 }
