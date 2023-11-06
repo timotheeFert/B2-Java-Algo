6f16fbf98281a3cf6b1d50c99994309d19ece554
timotheeFert <148551228+timotheeFert@users.noreply.github.com>
2023-11-06 15:01:15 +0100

    Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,23 +1,26 @@
 package fr.gns.app;
 
-	class Joueur {
-		private int numero ;
-		private String nom ;
-		private String pays ;
-		private int nbVictoires = 0 ;
+import java.util.Objects;
+
+public class Joueur {
+	private int numero ;
+	private String nom ;
+	private String pays ;
+	private int nbVictoires = 0 ; // Nombre de victoires remportées
 	public Joueur( int numero , String nom , String pays ){
-		super() ;
-		this.numero = numero ;
-		this.nom = nom ;
-		this.pays = pays ;
-		}
-	public Joueur( int numero , String nom , String pays , int nbVictoires ){
-		super() ;
-		this.numero = numero ;
-		this.nom = nom ;
-		this.pays = pays ;
-		this.nbVictoires = nbVictoires ;
+	super() ;
+	this.numero = numero ;
+	this.nom = nom ;
+	this.pays = pays ;
 	}
+	public Joueur( int numero , String nom , String pays , int nbVictoires ){
+	super() ;
+	this.numero = numero ;
+	this.nom = nom ;
+	this.pays = pays ;
+	this.nbVictoires = nbVictoires ;
+	}
+	
 	public int getNumero() {
 		return numero;
 	}
@@ -42,30 +45,30 @@ package fr.gns.app;
 	public void setNbVictoires(int nbVictoires) {
 		this.nbVictoires = nbVictoires;
 	}
-	
+	@Override
+	public boolean equals(Object obj) {
+		if (this.getNumero() == numero)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Joueur other = (Joueur) obj;
+		return nbVictoires == other.nbVictoires && Objects.equals(nom, other.nom) && numero == other.numero
+				&& Objects.equals(pays, other.pays);
+	}
+	public int compareTo(Joueur autreJoueur) {
+	    if (this.getNbVictoires() > autreJoueur.getNbVictoires()) {
+	        return 1;
+	    } else if (this.getNbVictoires() == autreJoueur.getNbVictoires()) {
+	        return 0;
+	    } else {
+	        return -1;
+	    }
+	}
+	@Override
 	public String toString() {
-		return "Joueur [numero=" + numero + ", nom=" + nom + ", pays=" + pays + ", nbVictoires=" + nbVictoires + "]";
+		return "numero = " + numero + ", nom = " + nom + ", pays = " + pays + ", nbVictoires = " + nbVictoires + "\n";
 	}
 	
-	 public boolean equals(Joueur autreJoueur) {
-	        if (this == autreJoueur) {
-	            return true; 
-	        }
-
-	        if (autreJoueur == null) {
-	            return false; 
-	        }
-
-	        return numero == autreJoueur.numero;
-	  }
-	 
-	 public int compareTo(Joueur autreJoueur) {
-		    if (this.getNbVictoires() > autreJoueur.getNbVictoires()) {
-		        return 1;
-		    } else if (this.getNbVictoires() == autreJoueur.getNbVictoires()) {
-		        return 0;
-		    } else {
-		        return -1;
-		    }
-		}
 }
